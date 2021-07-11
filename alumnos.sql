64b30ae845d1840d1395f6f2f5f3c595c771c5e9
Laura <laura.avalle@gmail.com>
2021-07-11 18:33:34 -0300

    que se yo!

---
+++ alumnos.sql
@@ -0,0 +1,21 @@
+create database if not exists alumnos;
+use alumnos;
+
+CREATE TABLE alumnos (
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(150),
+    apellido VARCHAR(150),
+    mail VARCHAR(255),
+	horarios VARCHAR (200),
+    cel VARCHAR(150),
+    foto VARCHAR (5000),
+    PRIMARY KEY (id)
+);
+
+insert into alumnos (nombre, apellido, mail, cel, foto) values
+('Laura', 'Avalle', 'laura.avalle@gmail.com', '1566517318', '1978-11-01 '); 
+
+SELECT 
+    *
+FROM
+    alumnos;
