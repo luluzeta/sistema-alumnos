53e8ba9f00e689390600a7d888d99f94283a48d3
Laura <laura.avalle@gmail.com>
2021-07-12 18:44:07 -0300

    Guardo ruta de foto

--- SRC/app.py
+++ SRC/app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 from flask import render_template
 from flask import request, redirect
@@ -43,6 +44,15 @@ def store():
     _celular = request.form['txtCel']
     _foto = request.files['txtFoto']
 
+    now = datetime.now()
+    print(now)
+    tiempo = now.strftime("%Y%H%M%S")
+    print(tiempo)
+
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
     sql = "INSERT INTO alumnos (nombre, apellido, mail, horarios, cel, foto) values (%s, %s, %s, %s, %s, %s);"
     datos = (_nombre, _apellido, _mail, _horarios, _celular, _foto.filename)
 
@@ -52,7 +62,7 @@ def store():
     cursor.execute(sql, datos)
     conn.commit()
 
-    return render_template('alumnos/index.html')
+    return redirect('/')
 
 
 if __name__ == '__main__':
